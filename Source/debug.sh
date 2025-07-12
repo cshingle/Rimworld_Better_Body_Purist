fc32a126fbff4f9475b67b4dc42435051932097f
caleb <caleb@becomingmachinic.com>
2025-07-12 14:35:57 -0500

    Updated for Rimworld 1.6

--- Source/debug.sh
+++ Source/debug.sh
@@ -2,9 +2,15 @@
 
 FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.7.2-api/ dotnet build *.csproj /property:Configuration=Debug
 
-rm -rf ../1.5/Assemblies/
-mkdir ../1.5/Assemblies/
+rm -rf ../1.6/Assemblies/
+mkdir ../1.6/Assemblies/
 
+ln -s $(realpath bin/Debug)/Zig158.BetterBodyPurist.dll $(realpath ../1.6/Assemblies/)/Zig158.BetterBodyPurist.dll
+ln -s $(realpath bin/Debug)/Zig158.BetterBodyPurist.pdb $(realpath ../1.6/Assemblies/)/Zig158.BetterBodyPurist.pdb
 
-ln -s $(realpath bin/Debug)/Zig158.BetterBodyPurist.dll $(realpath ../1.5/Assemblies/)/Zig158.BetterBodyPurist.dll
-ln -s $(realpath bin/Debug)/Zig158.BetterBodyPurist.pdb $(realpath ../1.5/Assemblies/)/Zig158.BetterBodyPurist.pdb
+rm -rf $RIMWORLD_MOD_DIR/BetterBodyPurist
+mkdir $RIMWORLD_MOD_DIR/BetterBodyPurist
+ln -s $(realpath ../)/LICENSE $(realpath $RIMWORLD_MOD_DIR/BetterBodyPurist)/LICENSE
+ln -s $(realpath ../)/About $(realpath $RIMWORLD_MOD_DIR/BetterBodyPurist)/About
+ln -s $(realpath ../)/1.5 $(realpath $RIMWORLD_MOD_DIR/BetterBodyPurist)/1.5
+ln -s $(realpath ../)/1.6 $(realpath $RIMWORLD_MOD_DIR/BetterBodyPurist)/1.6
--- Source/release.sh
+++ Source/release.sh
@@ -2,7 +2,17 @@
 
 FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.7.2-api/ dotnet build *.csproj /property:Configuration=Release
 
-rm -rf ../1.5/Assemblies/
-mkdir ../1.5/Assemblies/
+rm -rf ../1.6/Assemblies/
+mkdir ../1.6/Assemblies/
+cp bin/Release/Zig158.BetterBodyPurist.dll ../1.6/Assemblies/
 
-cp bin/Release/Zig158.BetterBodyPurist.dll ../1.5/Assemblies/
+rm -rf $RIMWORLD_MOD_DIR/BetterBodyPurist
+mkdir $RIMWORLD_MOD_DIR/BetterBodyPurist
+mkdir $RIMWORLD_MOD_DIR/BetterBodyPurist/About
+mkdir $RIMWORLD_MOD_DIR/BetterBodyPurist/1.5
+mkdir $RIMWORLD_MOD_DIR/BetterBodyPurist/1.6
+
+cp $(realpath ../)/LICENSE $(realpath $RIMWORLD_MOD_DIR/BetterBodyPurist)/LICENSE
+cp -r $(realpath ../)/About/* $(realpath $RIMWORLD_MOD_DIR/BetterBodyPurist)/About/
+cp -r $(realpath ../)/1.5/* $(realpath $RIMWORLD_MOD_DIR/BetterBodyPurist)/1.5/
+cp -r $(realpath ../)/1.6/* $(realpath $RIMWORLD_MOD_DIR/BetterBodyPurist)/1.6/
